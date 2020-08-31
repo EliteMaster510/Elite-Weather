9718a11f59ebe8ba7dcf4574bd8a63ff2ff73b39
EliteMaster510 <70373453+EliteMaster510@users.noreply.github.com>
2020-08-31 17:18:36 +0530

    Update views.py

--- EliteWeather/views.py
+++ EliteWeather/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 
 def home(request):
+    import json
+     import requests
+
+     api_requests = requests.get("http://dataservice.accuweather.com/currentconditions/v1/540155_PC?apikey=%09sGwdYxLUJfpyoK3coytpzZFXeHU2MfU5&details=true")
+
+     try:
+         api = json.loads(api_requests.content)
+     except Exception as e:
+         api = "Error..."
+    
     return render(request, 'home.html', {})
 
 def about(request):
